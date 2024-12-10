cb50c6b5f475751472b1c95b8886503b82a10223
AVGfx <avgfx15@gmail.com>
2024-12-10 09:40:36 +0530

    Create UserSchema with Mongoose

---
+++ server/MODELS/userSchema.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+// Declare the Schema of the Mongo model
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+//Export the model
+const UserSchema = mongoose.model('User', userSchema);
+
+export default UserSchema;
