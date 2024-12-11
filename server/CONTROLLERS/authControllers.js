1b93a3d1e0abb4794957c9917b744f31eec01355
AVGfx <avgfx15@gmail.com>
2024-12-11 21:10:10 +0530

    Create SignIn api Route in server

--- server/CONTROLLERS/authControllers.js
+++ server/CONTROLLERS/authControllers.js
@@ -1,5 +1,8 @@
 import bcryptjs from 'bcryptjs';
 
+// ~ Import jsonwebtoken
+import jwt from 'jsonwebtoken';
+
 import UserSchema from '../MODELS/userSchema.js';
 import { errorHandler } from '../MIDDLEWARE/errorMiddleware.js';
 
@@ -37,6 +40,51 @@ export const signUpController = async (req, res, next) => {
     await newUser.save();
 
     return res.status(201).json({ message: 'New User Created', newUser });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+// / SignIn Controlelr
+
+export const signInController = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === '' || password === '') {
+    return next(errorHandler(400, 'All Fields Are Required'));
+  }
+  try {
+    // % Check if user exist
+    const userExist = await UserSchema.findOne({ email });
+    if (!userExist) {
+      return next(errorHandler(404, 'User Not Exist'));
+    }
+
+    // % compare Hash Password
+    const checkPasswordValid = bcryptjs.compareSync(
+      password,
+      userExist.password
+    );
+    if (!checkPasswordValid) {
+      return next(errorHandler(404, 'Invalid Credentials'));
+    }
+    const loggedInUser = {
+      id: userExist.id,
+      username: userExist.username,
+      mobile: userExist.mobile,
+      email: userExist.email,
+    };
+
+    // * Create Token
+    const token = jwt.sign({ id: userExist.id }, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    });
+
+    return res
+      .status(200)
+      .cookie('access_token', token, { httpOnly: true })
+      .json(loggedInUser);
+
+    // % Create Token
   } catch (error) {
     return next(error);
   }
--- server/ROUTES/authRoutes.js
+++ server/ROUTES/authRoutes.js
@@ -1,8 +1,19 @@
 import express from 'express';
-import { signUpController } from '../CONTROLLERS/authControllers.js';
 
+// & Declare authRouter
 const authRouter = express.Router();
 
+// ~ Import Controllers
+import {
+  signUpController,
+  signInController,
+} from '../CONTROLLERS/authControllers.js';
+
+// & Routes
+// + Add New User Route
 authRouter.post('/signup', signUpController);
+
+// / Signin Route
+authRouter.post('/signin', signInController);
 
 export default authRouter;
