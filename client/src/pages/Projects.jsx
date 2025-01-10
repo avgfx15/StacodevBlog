c040560b6a5a4a4272759755ed4674201237c8e6
AVGfx <avgfx15@gmail.com>
2025-01-10 20:15:48 +0530

    Complete project page

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,5 +1,16 @@
+import CallToAction from '../components/CallToAction';
+
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className='min-h-screen flex flex-col gap-3 p-5 justify-center max-w-7xl items-center mx-auto'>
+      <h1 className='text-3xl font-semibold'>Projects</h1>
+      <p>
+        Build project for learning and enjoying to create in HTML, nodejs,
+        reactjs, mongodb etc...
+      </p>
+      <CallToAction />
+    </div>
+  );
 };
 
 export default Projects;
