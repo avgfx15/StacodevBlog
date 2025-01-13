1de94102c09eaef4487885f2daec4e25992100ae
AVGfx <avgfx15@gmail.com>
2025-01-13 08:37:12 +0530

    Ymlfile create for render deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import process from 'process';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   server: {
     proxy: {
-      '/api':
-        process.env.NODE_ENV === 'production'
-          ? 'https://stacodevblog.onrender.com/'
-          : 'http://localhost:3200',
+      '/api': import.meta.env.VITE_API_URL || 'http://localhost:3200',
       secure: false,
     },
   },
